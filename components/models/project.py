133d1bfbe7439cebb0d023064d0f18ed1f3aa9e6
Adrian Belmontes <ajbelmontes@miners.utep.edu>
2021-09-18 00:05:30 -0400

    added functions to workspace and projects

--- components/models/project.py
+++ components/models/project.py
@@ -1,5 +1,6 @@
-from datetime import datetime
 #from dataset import Dataset
+from datetime import datetime
+import os, shutil
 
 class Project:
     def __init__(self, name: str, c_time = datetime.now(), size = 0, datasets = []) -> None:
@@ -7,12 +8,26 @@ class Project:
         self.c_time = c_time     # creation time
         self.size = size         # size in bytes
         self.dataset = datasets
-'''
+        self.create_folder()
+    '''
     def add_dataset(self, new: Dataset) -> list:
         self.dataset.append(new)
         return self.dataset
 
     def del_datset(self, rm: Dataset) -> list:
+        rm.remove()
         self.dataset.remove(rm)
         return self.dataset
-'''
+    '''
+    def create_folder(self) -> str:
+        path = os.path.join(os.getcwd(), self.name)
+        os.mkdir(path)
+        return path
+    
+    def remove(self) -> bool:
+        try:
+            path = os.path.join(os.getcwd(), self.name)
+            shutil.rmtree(path)
+            return True
+        except:
+            return False
--- components/models/workspace.py
+++ components/models/workspace.py
@@ -1,15 +1,46 @@
 from project import Project
+import os, shutil
 
 class Workspace:
-    def __init__(self, name: str, location: list, projects = []) -> None:
+    def __init__(self, name:str, location:str, project=[]) -> None:
         self.name = name
-        self.location = location  # split before passing to avaoid os path complications
-        self.project = projects
+        self.location = location
+        self.project = project
+        self.cwd = os.getcwd()
+        self.work_dir()
 
-    def add_project(self, new: Project) -> list:
-        self.project.append(new)
+    # TODO: Ask Eyan what would be better
+    #def add_project(self, new:Project) -> list:
+        #self.project.append(new)
+    def add_project(self, name:str) -> list:
+        self.project.append(Project(name))
         return self.project
 
-    def del_project(self, rm: Project) -> list:
-        self.project.remove(rm)
+    def del_project(self, old:Project) -> list:
+        old.remove()
+        self.project.remove(old)
         return self.project
+
+    def work_dir(self) -> str:
+        path = os.path.join(self.location, self.name)
+        os.mkdir(path)
+        os.chdir(path)
+        return path
+
+    def close(self) -> bool:
+        try:
+            path = os.path.join(self.location, self.name)
+            os.chdir(self.cwd)
+            shutil.rmtree(path)
+            return True
+        except:
+            return False
+
+    def save(self) -> bool:
+        try:
+            path = os.path.join(self.location, self.name)
+            shutil.make_archive(path,'zip', path)
+            return True
+        except:
+            return False
+
