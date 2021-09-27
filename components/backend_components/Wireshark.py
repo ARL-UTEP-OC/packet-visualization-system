f9af9ded5020d4aaa52d36e994f31c8bb445bbc7
timmy <tlwilliams2@miners.utep.edu>
2021-09-27 16:42:17 -0600

    Add logic to determine Windows or Linux

--- components/backend_components/Wireshark.py
+++ components/backend_components/Wireshark.py
@@ -1,4 +1,10 @@
 import os
+import platform
 
 def openwireshark(path):
-    os.system('cd "C:\Program Files\Wireshark" & wireshark -r '+path)
+
+    if(platform.system()=="Linux"):
+        os.system('wireshark -r '+path)
+
+    elif(platform.system()=="Windows"):
+        os.system('cd "C:\Program Files\Wireshark" & wireshark -r '+path)
