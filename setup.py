3ea2e040dfba21631c0309a6934aeb93a6261520
Abraham Lomely <abraham-ba-18@hotmail.com>
2021-09-28 10:23:27 -0600

    hotfix2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="packetvisualization",
-    version="0.0.1",
+    version="0.0.2",
     author="team-1",
     author_email="hbarrazalo@miners.utep.edu",
     description="packet visualization",
