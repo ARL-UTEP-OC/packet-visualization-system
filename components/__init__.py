ca5372c4585f03eeeae29e3c142509d02176108a
Adrian Belmontes <ajbelmontes@miners.utep.edu>
2021-10-11 21:52:36 +0000

    Update __init__.py

--- components/__init__.py
+++ components/__init__.py
@@ -1,10 +1,7 @@
-from components.ui_components.startup_gui_redesign import StartupWindow
-import components.models.context.database_context as _context
-
-
-# from components.models.context.entities import Dataset, Pcap
-
 def run():
+    from components.ui_components.startup_gui_redesign import StartupWindow
+    import components.models.context.database_context as _context
+    # from components.models.context.entities import Dataset, Pcap
     _context.Base.metadata.create_all(bind=_context.engine, checkfirst=True)
     ui = StartupWindow()
     ui.run_program()
