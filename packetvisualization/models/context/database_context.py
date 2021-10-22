3ed6d26ae4acd28f32f689c06507eda85ffa9b68
Abraham Lomely <abraham-ba-18@hotmail.com>
2021-10-21 21:15:35 -0600

    mongo db implementation progress

--- packetvisualization/models/context/database_context.py
+++ packetvisualization/models/context/database_context.py
@@ -1,11 +1,26 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+import pymongo
+import subprocess
+from pymongo.database import Database
+from decouple import config
 
 
-Base = declarative_base()
-engine = create_engine('sqlite:///packets.sqlite', echo =True)
-# Base.metadata.create_all(bind=engine)  # here
+class DbContext:
+    db = Database
+    dbString = config('DBSTRING')
 
-Session = sessionmaker(bind=engine)
-session = Session()
+    def __init__(self):
+        client = pymongo.MongoClient(f'{self.dbString}?retryWrites=true&w=majority')
+        self.db = client.PracticumDB
+        print(self.db.list_collections())
+
+    def destroy_session(self):
+        # TODO: Fix this, until this is fixed we have to manually delete the DB data
+        # mongodump -d yourDB -c "your/colName" --out "-" --quiet > col.bson
+        subprocess.call([f'mongodump --uri {self.dbString} --out "-" --quiet > col.bson']);
+
+# Implementation example
+context = DbContext()
+packet = {'_id': 3, 'name': 'test-packet', 'meta': ['testing1', 'testing2']}
+
+packets_doc = context.db.Packets
+result = packets_doc.insert_one(packet)
