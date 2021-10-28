a5254120c389dc1f73b66d275c2f7b6ed50be030
LXvsqz <avasquez31@miners.utep.edu>
2021-10-28 14:39:36 -0600

    Added functions for DB Dump, DB Restore, and adding/deleting database from local machine

--- packetvisualization/backend_components/entity_operator.py
+++ packetvisualization/backend_components/entity_operator.py
@@ -1,10 +1,26 @@
 from packetvisualization.models.context.database_context import DbContext
 from pymongo import MongoClient, InsertOne, DeleteMany
 import json
+import os
 
 
 class EntityOperations:
     context = DbContext()
+
+    def create_db(self,workspace_name, client):  # create new DB when we have a new workspace
+        mydb = client[workspace_name]
+        return mydb
+
+    def remove_db(self, workspace_name, client):  # Use when we don't save workspace
+        client.drop_database(workspace_name)
+
+    def dump_db(self, workspace_name, save_dir):
+        # x = "mongodump --db " + workspace_name + " --out " + workspace_directory
+
+        os.system("mongodump --db " + workspace_name + " --out " + save_dir)
+
+    def restore_db(self, workspace_name, save_location):
+        os.system("mongorestore --db " + workspace_name + " --drop " + save_location)
 
     def fix_dictionary(self, d):  # Function to replace any key with '.' in name
         new = {}
@@ -14,10 +30,9 @@ class EntityOperations:
             new[k.replace('.', '-')] = v
         return new
 
-    def insert_packets(self, json_file, collection, dataset_name,
-                       pcap_name):  # take json with packet information and bulk insert into DB
+    def insert_packets(self, json_file, collection, dataset_name, pcap_name):  # take json with packet information and bulk insert into DB
         requesting = []
-        with open(json_file, encoding="ISO-8859-1") as f:
+        with open(json_file, encoding="ISO-8859-1") as f:  #
             packet_data = json.load(f)  # list of packets w/data as json object
             for jsonObj in packet_data:
                 jsonObj["parent_dataset"] = dataset_name
