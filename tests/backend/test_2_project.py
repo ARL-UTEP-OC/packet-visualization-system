fc9ed663d0abc9da6c646fe3c89d9f9d01889c75
Adrian <ajbelmontes@miners.utep.edu>
2021-11-01 14:03:36 -0400

    added some test classes, removed gui test classes

--- tests/backend/test_2_project.py
+++ tests/backend/test_2_project.py
@@ -1,17 +1,21 @@
-from packetvisualization.models.workspace import Workspace
+import hashlib
+import os
+import pytest
+
 from packetvisualization.models.project import Project
-import os, pytest, hashlib
+from packetvisualization.models.workspace import Workspace
 
 cwd = None
 w = None
 p1, p2 = None, None
 
+
 def test_create_project():
-    global w,cwd,p1,p2
+    global w, cwd, p1, p2
     cwd = os.getcwd()
     w = Workspace("testWorkspace2", cwd)
-    p1 = Project("testProject1", w.path,1632527017.653542)
-    p2 = Project("testProject2", w.path,1632527130.789377)
+    p1 = Project("testProject1", w.path, 1632527017.653542)
+    p2 = Project("testProject2", w.path, 1632527130.789377)
     assert p1.name == "testProject1"
     assert p2.name == "testProject2"
     assert p1.c_time == 1632527017.653542
@@ -21,15 +25,17 @@ def test_create_project():
     assert p1.dataset == []
     assert p2.dataset == []
     assert p1.path == os.path.join(cwd, ".testWorkspace2", "testProject1")
-    for p in [p1,p2]:
+    for p in [p1, p2]:
         assert os.path.isdir(os.path.join(cwd, ".testWorkspace2", p.name))
     assert w.project == []
 
+
 def test_add_project():
-    global w,p1,p2
+    global w, p1, p2
     assert [p1] == w.add_project(p1)
     assert [p1, p2] == w.add_project(p2)
     assert w.project == [p1, p2]
+
 
 def test_find_project():
     global w, p1, p2
@@ -37,15 +43,17 @@ def test_find_project():
     assert w.find_project("testProject2") == p2
     assert w.find_project("testProject3") == None
 
+
 def test_del_project():
-    global w,p1,p2
+    global w, p1, p2
     assert [p2] == w.del_project(p1)
     assert w.project == [p2]
     assert not os.path.isdir(os.path.join(cwd, ".testWorkspace2", "testProject1"))
 
+
 def test_save_project():
     global w
-    w.save() # testing saving up to projects
+    w.save()  # testing saving up to projects
     exported_files = [os.path.join(cwd, "testWorkspace2.zip"), os.path.join(cwd, ".testWorkspace2", "save.json")]
     exported_hash = "588a0a1700b334e9a2446e711c7aa4a5ffab2b70"
     for f in exported_files:
@@ -53,6 +61,7 @@ def test_save_project():
     with open(exported_files[1], 'rb') as f:
         data = f.read()
         assert exported_hash == hashlib.sha1(data).hexdigest()
+
 
 def test_save_project_overwrite():
     # make sure saving multiple times overwrites the previous save
@@ -66,7 +75,7 @@ def test_save_project_overwrite():
     with open(exported_files[1], 'rb') as f:
         data = f.read()
         assert exported_hash == hashlib.sha1(data).hexdigest()
-    
+
     w.del_project(p1)
     w.save()
     # back to old checksum
@@ -77,8 +86,9 @@ def test_save_project_overwrite():
         data = f.read()
         assert exported_hash == hashlib.sha1(data).hexdigest()
 
+
 def test_del():
-    global w,p1,p2
+    global w, p1, p2
     del w
     with pytest.raises(NameError):
         print(w)
