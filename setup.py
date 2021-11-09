625fd993aeefcd97b900a7d7bedbb14e7d432dd1
Adrian Belmontes <ajbelmontes@miners.utep.edu>
2021-11-09 03:49:06 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import setuptools
 
 setuptools.setup(
     name="packetvisualization",
-    version="0.0.8",
+    version="0.0.9",
     author="team-1",
     author_email="hbarrazalo@miners.utep.edu",
     description="packet visualization",
